834b03f26be3ccf3d2103070972594fe11ee31a8
guoweikuang <673411814@qq.com>
2018-04-08 17:06:49 +0800

    remove some

--- app_runner.py
+++ app_runner.py
@@ -4,7 +4,7 @@ from login.login import Login
 
 if __name__ == '__main__':
     login_url = "https://passport.weibo.cn/sso/login"
-    username = "17317540230"
-    password = "gwk2014081029"
+    username = ""
+    password = ""
     login = Login(login_url, username, password)
     login.login()
